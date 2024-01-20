a07d6efcac7a48940b37097e6ce97655d652e41d
Syed Asad Jamil <syedasad1410@gmail.com>
2024-01-20 21:10:20 +0500

    LAB 01 added

--- LAB 01/Exercise 3.c
+++ LAB 01/Exercise 3.c
@@ -4,13 +4,13 @@
 This Program Perform simple arithmetic operations like addition, subtraction, multiplication, division, remainder). */
 int main()
 {
-    int x,y;
-    x=99;
-    y=11;
-    printf("x+y=%d\n",x+y);
-    printf("x-y=%d\n",x-y);
-    printf("x*y=%d\n",x*y);
-    printf("x/y=%d\n",x/y);
-    printf("remainder=%d\n",x%y);
+    int x, y;
+    x = 99;
+    y = 11;
+    printf("x+y=%d\n", x + y);
+    printf("x-y=%d\n", x - y);
+    printf("x*y=%d\n", x * y);
+    printf("x/y=%d\n", x / y);
+    printf("remainder=%d\n", x % y);
     return 0;
 }
