bb734e2e7869271ded6416b31602904aa0938837
Syed Asad Jamil <syedasad1410@gmail.com>
2024-01-20 21:12:02 +0500

    LAB 01 added

--- LAB 01/Exercise 4.c
+++ LAB 01/Exercise 4.c
@@ -6,22 +6,22 @@ First one is by using a 3rd variable and 2nd is without 3rd variable. */
 int main()
 {
     /*with third variable*/
-    int a,b,c;
-    a=2;
-    b=10;
-    c=a;
-    a=b;
-    b=c;
-    printf("Value of a after swapping is:%d\n",a);
-    printf("Value of b after swapping is:%d\n",b);
+    int a, b, c;
+    a = 2;
+    b = 10;
+    c = a;
+    a = b;
+    b = c;
+    printf("Value of a after swapping is:%d\n", a);
+    printf("Value of b after swapping is:%d\n", b);
 
     /*without third variable*/
-    int n1,n2;
-    n1=70;
-    n2=90;
-    n1=n2;
-    n2=n1-20;
-    printf("Value of n1 after swapping is:%d\n",n1);
-    printf("Value of n2 after swapping is:%d\n",n2);
+    int n1, n2;
+    n1 = 70;
+    n2 = 90;
+    n1 = n2;
+    n2 = n1 - 20;
+    printf("Value of n1 after swapping is:%d\n", n1);
+    printf("Value of n2 after swapping is:%d\n", n2);
     return 0;
 }
