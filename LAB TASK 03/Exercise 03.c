3891e20b8665e1637711ae1f91a01c43db3257e8
Syed Asad Jamil <syedasad1410@gmail.com>
2024-02-25 11:31:45 +0500

    apply format

--- LAB TASK 03/Exercise 03.c
+++ LAB TASK 03/Exercise 03.c
@@ -5,16 +5,15 @@
 
 int main()
 {
-    long int i=0, product=1, num=19;
-    while(i<=19) //condition for iteration of loop.
+    long int i = 0, product = 1, num = 19;
+    while (i <= 19) // condition for iteration of loop.
     {
         i++;
-        if(i%2!=0) //remainder of odd number is not 0 when divided by 2.
-        product = product * i; //multiply the value of i with product on each iteration.
+        if (i % 2 != 0)            // remainder of odd number is not 0 when divided by 2.
+            product = product * i; // multiply the value of i with product on each iteration.
     }
 
-    printf("Product of Odd Integers is : %ld\n",product);
+    printf("Product of Odd Integers is : %ld\n", product);
 
     return 0;
 }
-
