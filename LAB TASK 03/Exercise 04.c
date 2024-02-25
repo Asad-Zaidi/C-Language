65b2d47d105682188422af0405aec3d0554fc996
Syed Asad Jamil <syedasad1410@gmail.com>
2024-02-25 11:32:11 +0500

    apply format

--- LAB TASK 03/Exercise 04.c
+++ LAB TASK 03/Exercise 04.c
@@ -5,21 +5,19 @@
 
 int main()
 {
-
-    int a,b,c,d,e,f,g;
+    int a, b, c, d, e, f, g;
     printf("Enter four digit number : ");
-    scanf("%d",&a);
-    b=a/1000; //after division last digit of number is displayed that is stored in b.
-    c=a%1000; //reminder is last three digits of number that is stored in c.
-    d=c/100;
-    e=c%100;
-    f=e/10;
-    g=e%10;
-    printf("%3d",b);
-    printf("%3d",d);
-    printf("%3d",f);
-    printf("%3d\n",g);
+    scanf("%d", &a);
+    b = a / 1000; // after division last digit of number is displayed that is stored in b.
+    c = a % 1000; // reminder is last three digits of number that is stored in c.
+    d = c / 100;
+    e = c % 100;
+    f = e / 10;
+    g = e % 10;
+    printf("%3d", b);
+    printf("%3d", d);
+    printf("%3d", f);
+    printf("%3d\n", g);
 
     return 0;
-
 }
